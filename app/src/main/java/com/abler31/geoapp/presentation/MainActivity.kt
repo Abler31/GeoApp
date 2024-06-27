5ea2177a590af0c3cce73e6b70a49693e50ee754
Abler31 <58187511+Abler31@users.noreply.github.com>
2024-06-27 17:28:27 +0300

    current location added

--- app/src/main/java/com/abler31/geoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/abler31/geoapp/presentation/MainActivity.kt
@@ -2,19 +2,26 @@ package com.abler31.geoapp.presentation
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.abler31.geoapp.R
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var map: MapView
-    private val vm: MainViewModel by viewModels()
+    lateinit var myLocationOverlay: MyLocationNewOverlay
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,7 +37,25 @@ class MainActivity : AppCompatActivity() {
             != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
         } else {
-            vm.startLocationUpdates()
+            getCurrentLocation()
         }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation()
+        }
+    }
+
+    private fun getCurrentLocation() {
+        myLocationOverlay = MyLocationNewOverlay(map)
+        myLocationOverlay.enableMyLocation()
+        myLocationOverlay.enableFollowLocation()
+        myLocationOverlay.setPersonAnchor(0.5f, 0.5f)
+        map.overlays.add(myLocationOverlay)
+        map.controller.animateTo(myLocationOverlay.myLocation)
+        map.controller.setZoom(15.5)
     }
 }
